826ede111affe86dab0116b57d29e2f119f6368a
Yaroslav <90201472+zazazaza4@users.noreply.github.com>
2023-09-26 11:16:59 +0300

    Update task_9.ts

--- js/task_9.ts
+++ js/task_9.ts
@@ -4,15 +4,12 @@ function add(x: number) {
   //створюємо функцію, яка приймає 'y' і додає до 'x'.
   function addNext(y: number) {
     x += y;
-
     // Повертаємо функцію 'addNext' для створення ланцюгу виклику
     return addNext;
   }
 
   // Перевизначаємо метод 'toString' для 'addNext', щоб повернути поточну суму 'x'.
-  addNext.prototype.toString = () => {
-    return x;
-  };
+  addNext.toString = () => x;
 
   // Повертаємо функцію addNext для всіх викликів, включаючи останній
   return addNext;
